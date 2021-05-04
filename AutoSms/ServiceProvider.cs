035e05dfff53f283ce2c308294131690ed945c63
Thomas Cowdrey <tommyarco@gmail.com>
2021-05-04 15:24:09 -0700

    Add project files.

---
+++ AutoSms/ServiceProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoSms
+{
+    public struct TextServiceProvider
+    {
+        public const string AttSms = "@txt.att.net";
+        public const string AttMms = "@mms.att.net";
+        public const string BoostMobileSms = "@sms.myboostmobile.com";
+        public const string BoostMobileMms = "@myboostmobile.com";
+        public const string SprintSms = "@messaging.sprintpcs.com";
+        public const string SprintMms = "@pm.sprint.com";
+        public const string TmobileSms = "@tmomail.net";
+        public const string TmobileMms = "@tmomail.net";
+        public const string VerizonSms = "@vtext.com";
+        public const string VerizonMms = "@vzwpix.com";
+        public const string VirginMobileSms = "@vmobl.com";
+        public const string VirginMobileMms = "@vmpix.com";
+        public const string XfinityMobileSms = "@vtext.com";
+        public const string XfinityMobileMms = "@mypixmessages.com";
+    }
+
+    public struct EmailServiceProvider
+    {
+        public const string Gmail = "smtp.gmail.com";
+        public const string Microsoft = "smtp.office365.com";
+        public const string Yahoo = "smtp.mail.yahoo.com";
+    }
+}
---
+++ AutoSms/TextMessage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace AutoSms
+{
+    public class TextMessage
+    {
+        public string To { get; set; }
+        public string Body { get; set; }
+        public string EmailAuthenticate { get; set; }
+        public string Token { get; set; }
+        public string TextType { get; set; }
+        public string TextServiceProvider { get; set; }
+        public string EmailServiceProvider { get; set; }
+
+        public void SendTextMessage(TextMessage textMessage, int portNumber = 465, bool UseSsl = true) 
+        {
+            var isNumber = long.TryParse(textMessage.To, out _);
+            if(isNumber is false)
+            {
+                throw new AutoSmsException("Please use only integers for the phone number.");
+            }
+            if (textMessage.To.Length != 10)
+            {
+                throw new AutoSmsException("Please make sure the phone number is 10 digits.");
+            }
+            if (!IsValidEmail(textMessage.EmailAuthenticate))
+            {
+                throw new AutoSmsException("Please use a valid email address.");
+            }
+
+            var toAddress = MailboxAddress.Parse(textMessage.To + textMessage.TextServiceProvider);
+            toAddress.Name = textMessage.To;
+            // check valid email, return error
+            var fromAddress = MailboxAddress.Parse(textMessage.EmailAuthenticate);
+            fromAddress.Name = textMessage.EmailAuthenticate;
+
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(fromAddress);
+            mailMessage.To.Add(toAddress);
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = textMessage.Body
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect(textMessage.EmailServiceProvider, portNumber, UseSsl);
+                smtpClient.Authenticate(textMessage.EmailAuthenticate, textMessage.Token);
+                smtpClient.Send(mailMessage);
+                smtpClient.Disconnect(true);
+            }
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public class AutoSmsException : Exception
+        {
+            public AutoSmsException()
+            {
+            }
+
+            public AutoSmsException(string message)
+                : base(message)
+            {
+            }
+
+            public AutoSmsException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+    }
+}
